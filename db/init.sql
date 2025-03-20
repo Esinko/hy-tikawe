21bfb91ff16c17388844c4515869d24e85439bba
Esinko <dev@esinko.net>
2025-03-20 16:09:57 +0200

    fix: database schema syntax
    Fixed database schema syntax and typos.

--- db/init.sql
+++ db/init.sql
@@ -1,19 +1,20 @@
 -- Clear database
-DELETE FROM challenge_categories;
-DELETE FROM assets;
-DELETE FROM attachments;
-DELETE FROM users;
-DELETE FROM profiles;
-DELETE FROM challenges;
-DELETE FROM submissions;
-DELETE FROM comments;
-DELETE FROM votes;
+DELETE FROM ChallengeCategories;
+DELETE FROM Assets;
+DELETE FROM Attachments;
+DELETE FROM Users;
+DELETE FROM Profiles;
+DELETE FROM Challenges;
+DELETE FROM Submissions;
+DELETE FROM Comments;
+DELETE FROM Votes;
+DELETE FROM VoteCounts;
 
 -- Default categories
-INSERT INTO challenge_categories (name) VALUES ("Least Lines of Javascript");
-INSERT INTO challenge_categories (name) VALUES ("One Line of Javascript");
-INSERT INTO challenge_categories (name) VALUES ("No Variable Declaration");
+INSERT INTO ChallengeCategories (name) VALUES ("Least Lines of Javascript");
+INSERT INTO ChallengeCategories (name) VALUES ("One Line of Javascript");
+INSERT INTO ChallengeCategories (name) VALUES ("No Variable Declaration");
 
 -- Create admin account
-INSERT INTO users (id, username, password_hash, require_new_password) VALUES (0, "admin", "", True);
-INSERT INTO profiles (id, user_id, description) VALUES (0, 0, "He who remains");
+INSERT INTO Users (id, username, password_hash, require_new_password) VALUES (0, "admin", "scrypt:32768:8:1$9YEUjvYO6LxacZfR$43915005f88112dff2ffd39726713e435aedcaa71a8e6e136f40c9edcd69ae16ba0a42691eef880a59c7fb14bac21e184cd9ad7a929e9fb4f445f65fbe6ae19e", True);
+INSERT INTO Profiles (id, user_id, description) VALUES (0, 0, "He who remains");
--- db/schema.sql
+++ db/schema.sql
@@ -1,84 +1,88 @@
 -- Assets and attachments
-CREATE TABLE assets (
+CREATE TABLE Assets (
     id INTEGER PRIMARY KEY,
     filename TEXT UNIQUE NOT NULL,
     value BLOB NOT NULL
 );
 
-CREATE TABLE attachments ( -- Challenges can have many assets
-    challenge_id INTEGER PRIMARY KEY
-    attachment_id INTEGER REFERENCES assets
-    FOREIGN KEY (challenge_id) REFERENCES challenges(id) ON DELETE CASCADE
-)
+CREATE TABLE Attachments ( -- Challenges can have many assets
+    id INTEGER PRIMARY KEY,
+    challenge_id INTEGER NOT NULL REFERENCES Challenges(id) ON DELETE CASCADE,
+    asset_id INTEGER NOT NULL REFERENCES Assets(id)
+);
 
 -- User Data
-CREATE TABLE users (
+CREATE TABLE Users (
     id INTEGER PRIMARY KEY,
     username TEXT UNIQUE NOT NULL,
     password_hash TEXT NOT NULL,
-    require_new_password BOOLEAN
+    require_new_password INTEGER NOT NULL DEFAULT 0
 );
 
-CREATE TABLE profiles (
+CREATE TABLE Profiles (
     id INTEGER PRIMARY KEY,
-    user_id INTEGER REFERENCES users NOT NULL,
+    user_id INTEGER NOT NULL REFERENCES Users(id) ON DELETE CASCADE,
     description TEXT,
-    profile_image_asset_id INTEGER REFERENCES assets,
-    FOREIGN KEY (profile_image_asset_id) REFERENCES assets(id) ON DELETE CASCADE
-    profile_banner_asset_id INTEGER REFERENCES assets
-    FOREIGN KEY (profile_banner_asset_id) REFERENCES assets(id) ON DELETE CASCADE
+    image_asset_id INTEGER REFERENCES Assets(id),
+    banner_asset_id INTEGER REFERENCES Assets(id)
 );
 
 -- Challenge categories
-CREATE TABLE challenge_categories (
+CREATE TABLE ChallengeCategories (
     id INTEGER PRIMARY KEY,
     name TEXT
-)
+);
 
 -- Challenges, submissions and comments
-CREATE TABLE challenges (
+CREATE TABLE Challenges (
     id INTEGER PRIMARY KEY,
     created INTEGER NOT NULL,
     title TEXT NOT NULL,
     body TEXT NOT NULL,
-    category INTEGER NOT NULL,
-    FOREIGN KEY (category) REFERENCES challenge_categories(id) ON DELETE CASCADE
-    author_id INTEGER NOT NULL
-    FOREIGN KEY (author_id) REFERENCES users(id)
-)
+    category INTEGER NOT NULL REFERENCES ChallengeCategories(id) ON DELETE CASCADE,
+    author_id INTEGER NOT NULL REFERENCES Users(id)
+);
 
-CREATE TABLE submissions (
+CREATE TABLE Submissions (
     id INTEGER PRIMARY KEY,
     created INTEGER NOT NULL,
-    challenge_id INTEGER NOT NULL
-    FOREIGN KEY (challenge_id) REFERENCES challenges(id) ON DELETE CASCADE
+    challenge_id INTEGER NOT NULL REFERENCES Challenges(id) ON DELETE CASCADE,
     title TEXT NOT NULL,
-    body TEXT NOT NULL
-    solution_asset_id INTEGER NOT NULL
-    FOREIGN KEY (solution_asset_id) REFERENCES assets(id) ON DELETE CASCADE
+    body TEXT NOT NULL,
+    solution_asset_id INTEGER NOT NULL REFERENCES Assets(id) ON DELETE CASCADE,
     author_id INTEGER NOT NULL
-)
+);
 
-CREATE TABLE comments (
+CREATE TABLE Comments (
     id INTEGER PRIMARY KEY,
     created INTEGER NOT NULL,
-    challenge_id INTEGER NOT NULL
-    FOREIGN KEY (challenge_id) REFERENCES challenges(id) ON DELETE CASCADE
-    body TEXT NOT NULL
-    author_id INTEGER NOT NULL
-    FOREIGN KEY (author_id) REFERENCES users(id)
-)
+    challenge_id INTEGER NOT NULL REFERENCES Challenges(id) ON DELETE CASCADE,
+    body TEXT NOT NULL,
+    author_id INTEGER NOT NULL REFERENCES Users(id)
+);
 
 -- Votes, 3 possible references
-CREATE TABLE votes (
-    id INTEGER PRIMARY KEY
+CREATE TABLE Votes (
     id INTEGER PRIMARY KEY,
-    challenge_id INTEGER REFERENCES challenges(id) ON DELETE CASCADE,
-    submission_id INTEGER REFERENCES submissions(id) ON DELETE CASCADE,
-    comment_id INTEGER REFERENCES comments(id) ON DELETE CASCADE,
+    challenge_id INTEGER REFERENCES Challenges(id) ON DELETE CASCADE,
+    submission_id INTEGER REFERENCES Submissions(id) ON DELETE CASCADE,
+    comment_id INTEGER REFERENCES Comments(id) ON DELETE CASCADE,
+    voter_id INTEGER REFERENCES Users(id) ON DELETE CASCADE,
     CHECK (
         (challenge_id IS NOT NULL AND submission_id IS NULL AND comment_id IS NULL) OR
         (challenge_id IS NULL AND submission_id IS NOT NULL AND comment_id IS NULL) OR
         (challenge_id IS NULL AND submission_id IS NULL AND comment_id IS NOT NULL)
     )
-)
+);
+
+CREATE TABLE VoteCounts ( -- Once and a while updated count based on the votes
+    challenge_id INTEGER REFERENCES Challenges(id) ON DELETE CASCADE,
+    submission_id INTEGER REFERENCES Submissions(id) ON DELETE CASCADE,
+    comment_id INTEGER REFERENCES Comments(id) ON DELETE CASCADE,
+    count INTEGER NOT NULL,
+    CHECK (
+        (challenge_id IS NOT NULL AND submission_id IS NULL AND comment_id IS NULL) OR
+        (challenge_id IS NULL AND submission_id IS NOT NULL AND comment_id IS NULL) OR
+        (challenge_id IS NULL AND submission_id IS NULL AND comment_id IS NOT NULL)
+    )
+);
